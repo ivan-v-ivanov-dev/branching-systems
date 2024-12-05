124a2a123ec35aa4710f13b426a73179477201c8
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2024-12-05 20:42:53 +0200

    Add TeamResponse and ProjectResponse

---
+++ models/src/main/java/com/personal/model/model/ProjectResponse.java
@@ -0,0 +1,17 @@
+package com.personal.model.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder(toBuilder = true)
+public class ProjectResponse {
+
+    private String name;
+
+    private String description;
+}
