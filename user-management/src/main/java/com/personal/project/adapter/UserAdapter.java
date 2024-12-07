82ef9f279ef5d1e73c3718c8fc029afdf71c1ed3
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2024-12-07 16:46:16 +0200

    Remove tema field

--- user-management/src/main/java/com/personal/project/adapter/UserAdapter.java
+++ user-management/src/main/java/com/personal/project/adapter/UserAdapter.java
@@ -11,7 +11,6 @@ public interface UserAdapter {
     @Mapping(target = "username", expression = "java(user.getUsername())")
     @Mapping(target = "firstName", expression = "java(user.getFirstName())")
     @Mapping(target = "lastName", expression = "java(user.getLastName())")
-    @Mapping(target = "team", expression = "java(user.getTeam())")
     @Mapping(target = "role", expression = "java(user.getRole().getName())")
     UserResponse fromUserToUserResponse(User user);
 }
