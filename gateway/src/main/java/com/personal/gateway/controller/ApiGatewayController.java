32566b7734443172a01036fb4443583729085b15
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2024-12-08 11:21:47 +0200

    Add TODOs

--- gateway/src/main/java/com/personal/gateway/controller/ApiGatewayController.java
+++ gateway/src/main/java/com/personal/gateway/controller/ApiGatewayController.java
@@ -17,27 +17,32 @@ public class ApiGatewayController {
     private final RoleService roleService;
     private final UserService userService;
 
+    //TODO: Restrict for CEO
     @GetMapping("/roles")
     public List<String> findAllRoles() {
         return roleService.findAllRoles();
     }
 
+    //TODO: Restrict for CEO
     @PostMapping("/role/{role}")
     public RoleGatewayRp createRole(@PathVariable("role") String role) {
         return roleService.create(role);
     }
 
+    //TODO: Restrict for CEO
     @PutMapping("/role/{oldName}/{newName}")
     public boolean renameRole(@PathVariable("oldName") String oldName,
                               @PathVariable("newName") String newName) {
         return roleService.rename(oldName, newName);
     }
 
+    //TODO: Restrict for CEO
     @DeleteMapping("/role/{role}")
     public boolean deleteRole(@PathVariable("role") String role) {
         return roleService.delete(role);
     }
 
+    //TODO: Restrict for CEO
     @GetMapping("/role/{role}/users")
     public List<UserGatewayRp> findAllRoleUsers(@PathVariable("role") String role) {
         return userService.findAllRoleUsers(role);
