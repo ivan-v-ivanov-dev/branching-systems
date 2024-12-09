b8af31e3f340e11eef78bc8f520c72a91b09a9f5
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2024-12-09 12:10:44 +0200

    Add constructor

--- vacation-management/src/main/java/com/personal/vacation/service/VacationListener.java
+++ vacation-management/src/main/java/com/personal/vacation/service/VacationListener.java
@@ -4,23 +4,26 @@ import com.personal.vacation.adapter.VacationAdapter;
 import com.personal.vacation.service.contract.VacationService;
 import com.social.kafka.messages.VacationMessage;
 import com.social.kafka.messages.contract.KafkaMessage;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
-@NoArgsConstructor(force = true)
 @Slf4j
 public class VacationListener {
 
+    public VacationListener(VacationService vacationService,
+                            VacationAdapter vacationAdapter,
+                            @Value("${spring.kafka.topic.name.vacation}") String newVacationTopic) {
+        this.vacationService = vacationService;
+        this.vacationAdapter = vacationAdapter;
+        this.newVacationTopic = newVacationTopic;
+    }
+
     private final VacationService vacationService;
     private final VacationAdapter vacationAdapter;
-    @Value("${spring.kafka.topic.name.vacation}")
-    String newVacationTopic;
+    private final String newVacationTopic;
 
     @KafkaListener(topics = "${spring.kafka.topic.name.vacation}", groupId = "${spring.kafka.group.id}")
     public void listener(KafkaMessage kafkaMessage) {
