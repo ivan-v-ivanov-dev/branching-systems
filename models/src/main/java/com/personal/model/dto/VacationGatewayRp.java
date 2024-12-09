8f77172cd91aa6625e97be43116fc9a6a7ced085
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2024-12-09 10:53:43 +0200

    Add VacationGatewayRp

---
+++ models/src/main/java/com/personal/model/dto/VacationGatewayRp.java
@@ -0,0 +1,29 @@
+package com.personal.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder(toBuilder = true)
+public class VacationGatewayRp {
+
+    private String type;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    private LocalDate submittedOn;
+
+    private boolean halfDay;
+
+    private boolean approved;
+
+    private String list;
+}
